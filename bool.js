34e9768e57fe9cdf480008737c60f5d2762d3f3b
RumeoLola <rumeomololaz@gmail.com>
2022-06-02 03:56:23 -0400

    boolean and conditional executaion and console method(s)

---
+++ bool.js
@@ -0,0 +1,10 @@
+var vaccinated = true
+var notvaxed = false
+
+if(vaccinated)  {
+    console.log("You are safe")
+}
+
+else    {
+    console.log("Get vaxed man")
+}
---
+++ debugconsole.js
@@ -0,0 +1,5 @@
+/*new array*/
+var animals = ["dog", "cow", "chicken", "cat"]
+
+// tabulate the array in the console
+console.table(animals)
